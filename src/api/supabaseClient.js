7105ab933467536c6eb4827c26c81723c7a7a8ee
kamskills <kamskills@gmail.com>
2026-09-16 17:45:03 +0200

    Update supabaseClient.js

--- src/api/supabaseClient.js
+++ src/api/supabaseClient.js
@@ -13,7 +13,8 @@ export const supabase = createClient(url || "", anonKey || "", {
   auth: {
     persistSession: true,
     autoRefreshToken: true,
-    detectSessionInUrl: true
+    detectSessionInUrl: true,
+    flowType: "pkce"
   }
 });
 
